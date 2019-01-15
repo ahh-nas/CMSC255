cc31fe7b5a26f0cfab16374a6d087f497a7369e5
Mohammad Zafar <zafarma2@vcu.edu>
2019-01-15 13:32:32 -0500

    Add files via upload

---
+++ HelloPrinter.java
@@ -0,0 +1,28 @@
+/*
+Lab 1 Assignment
+
+This is the first assignment in our lab on 1/15/19
+
+Mohammad Zafar
+1/15/19
+CMSC 255 002
+*/
+
+public class HelloPrinter
+{
+	public static void printHeading()
+	{
+		System.out.println("Mohammad Zafar");
+		System.out.println("Lab 1");
+		System.out.println("This is the first Lab assignment");
+		System.out.println("1/15/19");
+		System.out.println("CMSC 255 002");
+		System.out.println("");
+	}
+
+	public static void main(String args[])
+	{
+		printHeading();
+		System.out.println("Hello");
+	}
+}
